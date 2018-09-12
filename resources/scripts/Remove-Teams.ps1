e541948c94a01bdd1cf961ffba4a63f9fadd5ac7
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-09-12 10:22:04 +0200

    addded remove teams in resource

---
+++ resources/scripts/Remove-Teams.ps1
@@ -0,0 +1,12 @@
+﻿$Folders = Get-ChildItem "C:\Users"
+
+ForEach ($Folder in $Folders) {
+    If (!(($Folder.Name -eq "defaultuser0") -or ($Folder.Name -eq "Public"))) {
+        Start-Process -FilePath "$($Folder.FullName)\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList '--uninstall -s' -Wait
+        Remove-Item -Path "$($Folder.FullName)\Desktop\Microsoft Teams.lnk" -Force 
+        Remove-Item -Path "$($Folder.FullName)\AppData\Local\Microsoft\Teams" -Recurse -Force 
+        Remove-Item -Path "$($Folder.FullName)\AppData\Local\SquirrelTemp" -Recurse -Force
+        Remove-Item -Path "$($Folder.FullName)\AppData\Roaming\Microsoft\Teams" -Recurse -Force 
+        Remove-Item -Path "$($Folder.FullName)\Local\Microsoft\TeamsMeetingAddin" -Recurse -Force
+    }
+}
