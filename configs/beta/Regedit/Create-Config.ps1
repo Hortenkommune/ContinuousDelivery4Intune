d6a256dee01fdad889b5a90e2aec4b39453a6fbe
subsequent <fanuelsen@mac.com>
2018-09-12 09:15:51 +0200

    Added Telemetry Changes

--- configs/beta/Regedit/Create-Config.ps1
+++ configs/beta/Regedit/Create-Config.ps1
@@ -48,6 +48,11 @@
         URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/ConnectionsTab.reg"
         detection = "[bool]((Get-ItemPropertyValue -Path `"REGISTRY::HKEY_USERS\.DEFAULT\Software\Policies\Microsoft\Internet Explorer\Control Panel`" -Name ConnectionsTab) -eq 1)"
         Type = "HKCU"
+    },
+    @{
+        URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/Telemetry.reg"
+        detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\DataCollection`" -Name AllowDeviceNameInTelemetry) -eq 1)"
+        Type = "HKLM"
     }
 )
 $regfiles | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
