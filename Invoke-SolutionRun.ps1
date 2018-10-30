1b8851875cf98f2a7a3973252f5b8b72cee51a12
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-10-30 11:42:52 +0100

    Added ISR

---
+++ Invoke-SolutionRun.ps1
@@ -0,0 +1,8 @@
+﻿$CreateCfgFiles = Get-ChildItem $PSScriptRoot -Recurse | Where-Object { $_.Name -eq "Create-Config.ps1" }
+
+ForEach ($file in $CreateCfgFiles) {
+    Start-Process "powershell.exe" -ArgumentList "-Executionpolicy Bypass -File `"$($file.FullName)`"" -Wait
+}
+
+$CreateScriptFile = "$PSScriptRoot\Install\Create-Scripts.ps1"
+Start-Process "powershell.exe" -ArgumentList "-Executionpolicy Bypass -File `"$CreateScriptFile`"" -Wait
