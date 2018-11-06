2b06fc92a8d41e94d2d3f72d65510f6739c1cc99
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-11-06 12:09:51 +0100

    new helpfile

---
+++ Invoke-BetaCfgRun.ps1
@@ -0,0 +1,6 @@
+﻿$CreateCfgFiles = Get-ChildItem "$PSScriptRoot\configs\beta" -Recurse | Where-Object { $_.Name -eq "Create-Config.ps1" }
+
+ForEach ($file in $CreateCfgFiles) {
+    $Proc = Start-Process "powershell.exe" -ArgumentList "-Executionpolicy Bypass -File `"$($file.FullName)`"" -PassThru
+    $ProcIDs += $Proc.Id
+}
