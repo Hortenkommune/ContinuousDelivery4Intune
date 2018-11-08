235d868f2465596a2f1ab94e5dc9a48e05c0f02d
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2018-11-08 10:20:44 +0100

    Added icon

--- configs/prod.hovos/IconsCfg/Create-Config.ps1
+++ configs/prod.hovos/IconsCfg/Create-Config.ps1
@@ -8,6 +8,11 @@
         URI  = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/ico/Office_portal.ico"
         Name = "Office_portal.ico"
         Mode = "Install"
+    },
+    @{
+        URI  = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/ico/Office_portal.ico"
+        Name = "Office_portal.ico"
+        Mode = "Install"
     }
 )
 $Icons | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
