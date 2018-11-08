4d71a04be4568e486ce5c0370cc495ee70938ed6
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2018-11-08 10:22:05 +0100

    Removed icons

--- configs/prod.hovos/IconsCfg/Create-Config.ps1
+++ configs/prod.hovos/IconsCfg/Create-Config.ps1
@@ -8,11 +8,6 @@
         URI  = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/ico/Office_portal.ico"
         Name = "Office_portal.ico"
         Mode = "Install"
-    },
-    @{
-        URI  = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/ico/Office_portal.ico"
-        Name = "Office_portal.ico"
-        Mode = "Install"
     }
 )
 $Icons | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
