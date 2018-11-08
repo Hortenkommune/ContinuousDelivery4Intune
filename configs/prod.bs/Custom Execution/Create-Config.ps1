268338f76d40a96eeaca4b937b8350da4ab0cf90
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2018-11-08 13:37:01 +0100

    Trying again

--- configs/prod.bs/Custom Execution/Create-Config.ps1
+++ configs/prod.bs/Custom Execution/Create-Config.ps1
@@ -38,12 +38,11 @@
                 Execute   = "powershell.exe"
                 Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Run-PwrcfgWakeTimers.ps1"
             }
-        #)
-      #  Detection      = @(
-       #     @{
-        #        Rule = "[bool](`$False)"              
-                #Rule = "`$OP = (powercfg /query SCHEME_CURRENT 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d);[bool](`$OP -like `"*Current AC Power Setting Index: 0x00000001*`") -and (`$OP -like `"*Current DC Power Setting Index: 0x00000001*`")"
-          #  }
+        )
+        Detection      = @(
+            @{
+                Rule = "[bool](`$False)"              
+            }
         )
     }
 )
