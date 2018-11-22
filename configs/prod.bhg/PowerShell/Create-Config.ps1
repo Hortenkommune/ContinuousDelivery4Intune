93e4419a0361d71824f96aea99f205206b5603c3
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-11-22 13:14:32 +0100

    typo

--- configs/prod.bhg/PowerShell/Create-Config.ps1
+++ configs/prod.bhg/PowerShell/Create-Config.ps1
@@ -3,7 +3,7 @@
         Name      = "Remove VNC from Start Menu"
         Command   = "Remove-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC' -Recurse -Force"
         Detection = "[bool](!(Test-Path -Path `"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC`"))"
-    }
+    },
     @{
         Name      = "Add Restart-Computer every night"
         Command   = "Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-Command Restart-Computer -Force') -Trigger (New-ScheduledTaskTrigger -Daily -At 09:00pm) -User 'SYSTEM' -RunLevel Highest -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -WakeToRun) -TaskName 'Nightly Reboot' -Description 'Restart Computer nightly'"
