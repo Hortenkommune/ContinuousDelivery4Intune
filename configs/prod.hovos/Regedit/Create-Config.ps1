e4a932beca410c4dc95899c7c48c9dc63b81a502
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2018-11-29 14:16:56 +0100

    Removed Onedrive Registry Settings

--- configs/prod.hovos/Regedit/Create-Config.ps1
+++ configs/prod.hovos/Regedit/Create-Config.ps1
@@ -1,20 +1,5 @@
 ﻿$regfiles = @(
     @{
-        URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/DisableFileSyncNGSC.reg"
-        detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\Software\Policies\Microsoft\Windows\OneDrive`" -Name DisableFileSyncNGSC) -eq 0)"
-        Type = "HKLM"
-    },
-    @{
-        URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/SilentAccountConfig.reg"
-        detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\Software\Policies\Microsoft\OneDrive`" -Name SilentAccountConfig) -eq 1)"
-        Type = "HKLM"
-    },
-    @{
-        URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/FilesOnDemand.reg"
-        detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\Software\Policies\Microsoft\OneDrive`" -Name FilesOnDemandEnabled) -eq 1)"
-        Type = "HKLM"
-    },
-    @{
         URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/DisableFirstRunIE.reg"
         detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\Software\Microsoft\Internet Explorer\Main`" -Name DisableFirstRunCustomize) -eq 1)"
         Type = "HKLM"
@@ -32,11 +17,6 @@
     @{
         URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/TrustedSites.reg"
         detection = "[bool](Test-Path -Path `"REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\kommune.no\adfs.horten`")"
-        Type = "HKCU"
-    },
-    @{
-        URL = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/regfiles/EnableADAL.reg"
-        detection = "[bool]((Get-ItemPropertyValue -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\OneDrive -Name EnableADAL) -eq 1)"
         Type = "HKCU"
     },
     @{
