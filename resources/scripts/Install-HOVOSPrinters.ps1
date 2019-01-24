a37931840ca7e4f03e8075f1c0d2ec9496982b85
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-01-24 13:30:30 +0100

    Deployed printers to hovos

---
+++ resources/scripts/Install-HOVOSPrinters.ps1
@@ -0,0 +1,27 @@
+﻿$InstallPath = $env:PROGRAMFILES + "\HK-ELEV"
+$PrinterDriverInf = "Cnp60MA64.INF"
+$InstallDriverPath = $InstallPath + "\" + $PrinterDriverInf
+$ZipFile = $env:windir + "\temp\gpcl6.zip"
+$PrinterDriver = "Canon Generic PCL6 Driver"
+ 
+$Printers = @(
+    @{
+        Name = "Rom 204 HOVOS"
+        IP   = "10.85.200.54"
+    }, 
+    @{
+        Name = "Rom 121 HOVOS"
+        IP   = "10.85.200.49"
+    }
+)
+
+Expand-Archive -Path $ZipFile -DestinationPath $InstallPath -Force
+Start-Process -FilePath "pnputil.exe" -ArgumentList "/Add-Driver `"$InstallDriverPath`"" -Wait
+Add-PrinterDriver -Name $PrinterDriver
+
+Foreach ($Printer in $Printers) {
+    Add-PrinterPort -PrinterHostAddress $Printer.IP -Name $Printer.Name
+    Add-Printer -Name $Printer.Name -PortName $Printer.Name -DriverName $PrinterDriver
+}    
+
+Remove-Item -Path $InstallPath -Force -Recurse
