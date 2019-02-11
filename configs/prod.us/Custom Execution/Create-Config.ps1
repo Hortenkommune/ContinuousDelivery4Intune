5b5669d176b96ecd7e7a582501f1c09cc2563ec2
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-02-11 13:43:11 +0100

    added eksamen ting

--- configs/prod.us/Custom Execution/Create-Config.ps1
+++ configs/prod.us/Custom Execution/Create-Config.ps1
@@ -98,6 +98,27 @@
                 Rule = "`$OP = (powercfg /query SCHEME_CURRENT 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d);[bool](`$OP -like `"*Current AC Power Setting Index: 0x00000001*`") -and (`$OP -like `"*Current DC Power Setting Index: 0x00000001*`")"
             }
         )
+    },
+    @{
+        Name           = "Set up eksamen printers"
+        wrkDir         = "C:\Windows\Temp"
+        FilesToDwnload = @(
+            @{
+                FileName = "PEROLESSCRIPT.ps1"
+                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/scripts/Run-PER.ps1"
+            }
+        )
+        Execution      = @(
+            @{
+                Execute   = "powershell.exe"
+                Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Run-PER.ps1"
+            }
+        )
+        Detection      = @(
+            @{
+                Rule = "[bool](Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username | Where-Object { $_ -like `"*eksamen*`" })"
+            }
+        )
     }
 )
 $CustomExec | ConvertTo-Json -Depth 4 -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
