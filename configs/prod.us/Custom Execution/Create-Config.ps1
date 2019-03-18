453d2a6b298c352c62e1dd94d8751193f405676b
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-03-18 09:08:42 +0100

    +sent beta2prod

--- configs/prod.us/Custom Execution/Create-Config.ps1
+++ configs/prod.us/Custom Execution/Create-Config.ps1
@@ -98,6 +98,30 @@
                 Rule = "`$OP = (powercfg /query SCHEME_CURRENT 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d);[bool](`$OP -like `"*Current AC Power Setting Index: 0x00000001*`") -and (`$OP -like `"*Current DC Power Setting Index: 0x00000001*`")"
             }
         )
+    },
+    @{
+        Name           = "Set up eksamen printers"
+        wrkDir         = "C:\Windows\Temp"
+        FilesToDwnload = @(
+            @{
+                FileName = "Install-EksamenPrinters.ps1"
+                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/scripts/Install-EksamenPrinters.ps1"
+            }
+        )
+        Execution      = @(
+            @{
+                Execute   = "powershell.exe"
+                Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Install-EksamenPrinters.ps1"
+            }
+        )
+        Detection      = @(
+            @{
+                Rule = "[bool](!((Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username) -like `"*eksamen*`"))"
+            },
+            @{
+                Rule = "(!(Get-Printer | Where-Object { @('2FL02588', '2FL07038', 'QLC31644', 'XVC08019', 'XVF14345', 'QNW11407') -contains `$_.Name }))"
+            }
+        )
     }
 )
 $CustomExec | ConvertTo-Json -Depth 4 -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
