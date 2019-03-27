5cc4aaf0f52a712ccd2af8ce831be6c414a35a08
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2019-03-27 12:18:39 +0100

    Added GeoGebra to US Prod

--- configs/prod.us/Shortcuts/Create-Config.ps1
+++ configs/prod.us/Shortcuts/Create-Config.ps1
@@ -85,6 +85,16 @@
         IconFileandType = "C:\Windows\ICO\digres.ico"
         Description     = "Digitale Ressurser"
         Mode            = "Install"
+    },
+    @{
+        Name            = "GeoGebra Online"
+        Type            = "lnk"
+        Path            = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+        Arguments       = "https://www.geogebra.org/graphing"
+        WorkingDir      = "C:\Program Files (x86)\Google\Chrome\Application"
+        IconFileandType = "C:\Windows\ICO\geogebra.ico"
+        Description     = "GeoGebra Online"
+        Mode            = "Install"
     }
 )
 $Shortcuts | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
