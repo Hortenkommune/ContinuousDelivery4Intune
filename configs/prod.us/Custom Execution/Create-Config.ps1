1bcae0de7d6e0c3b23c7c0a4f5728bda69a381b0
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2019-04-26 14:01:20 +0200

    Added remove eksamens printers

--- configs/prod.us/Custom Execution/Create-Config.ps1
+++ configs/prod.us/Custom Execution/Create-Config.ps1
@@ -100,32 +100,26 @@
         )
     },
     @{
-        Name           = "Set up eksamen printers"
+        Name           = "Remove Eksamens Printers Not Working"
         wrkDir         = "C:\Windows\Temp"
         FilesToDwnload = @(
             @{
-                FileName = "Install-EksamenPrinters.ps1"
-                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/scripts/Install-EksamenPrinters.ps1"
-            },
-            @{
-                FileName = "pcl6.zip"
-                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/bin/pcl6.zip"
+                FileName = "Remove-EksamenPrinters.ps1"
+                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/scripts/Remove-EksamenPrinters.ps1"
             }
         )
         Execution      = @(
             @{
                 Execute   = "powershell.exe"
-                Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Install-EksamenPrinters.ps1"
+                Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Remove-EksamenPrinters.ps1"
             }
         )
         Detection      = @(
             @{
                 Rule = "[bool](!((Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username) -like `"*eksamen*`"))"
-            },
-            @{
-                Rule = "(!(Get-Printer | Where-Object { @('2FL02588', '2FL07038', 'QLC31644', 'XVC08019', 'XVF14345', 'QNW11407') -contains `$_.Name }))"
             }
         )
     }
+    
 )
 $CustomExec | ConvertTo-Json -Depth 4 -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
