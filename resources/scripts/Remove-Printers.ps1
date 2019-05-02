877e4e27d4e67613eeb178f02765d99a9440d0d3
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-05-02 15:30:15 +0200

    fixed rm print

--- resources/scripts/Remove-Printers.ps1
+++ resources/scripts/Remove-Printers.ps1
@@ -3,6 +3,7 @@ $PrintersToRm = @('2FL02588','2FL07038','QLC31644','XVC08019','XVF14345','QNW114
 
 Foreach ($Printer in $PrintersToRm) {
     If ($Printers.Name -contains $Printer.Name) {
-    Write-Host "remove $Printer.Name"
+        Remove-Printer -Name $Printer.Name
+        Remove-PrinterPort -Name $Printer.Name
     }
 }
