eff8b5ff77f652d9b22dfc779fca82c9a334ee61
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2019-05-02 13:51:44 +0200

    Added New-Install-EksamenPrinters.ps1

---
+++ resources/scripts/Install-EksamenPrinters.ps1
@@ -0,0 +1,93 @@
+$Username = Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username
+$Username = $Username.Replace("SKOLE\", "")
+
+$Printers = @(
+    @{
+        Name     = "2FL02588"
+        School   = "Borre"
+        Server   = "10.85.207.8"
+    },
+    @{
+        Name     = "2FL07038"
+        School   = "Borre"
+        Server   = "10.85.207.8"
+    },
+    @{
+        Name     = "QLC31644"
+        School   = "Holtan"
+        Server   = "10.85.207.8" 
+    },
+    @{
+        Name     = "XVC08019"
+        School   = "Holtan"
+        Server   = "10.85.207.8"
+    },
+    @{
+        Name     = "XVF14345"
+        School   = "Oreronningen"
+        Server   = "10.85.207.8"
+    },
+    @{
+        Name     = "QNW11407"
+        School   = "Oreronningen"
+        Server   = "10.85.207.8"
+    }
+)
+ 
+$holtan = @()
+@(001..199) | ForEach-Object {
+    $_ = $_.ToString("000")
+    $holtan += "eksamen" + $_
+}
+$borre = @()
+@(200..399) | ForEach-Object {
+    $_ = $_.ToString("000")
+    $borre += "eksamen" + $_
+}
+$oreronningen = @()
+@(400..599) | ForEach-Object {
+    $_ = $_.ToString("000")
+    $oreronningen += "eksamen" + $_
+}
+ 
+if ($holtan -contains $username) {
+    $Printer = $Printers | Where-Object {$_.school -eq 'holtan'}
+    foreach ($p in $Printer) {
+        if ($Printers.Name -contains $Printer.Name) {
+            Write-Host "$($p.name) Exist, Skipping"
+        }
+        else {
+            Invoke-Command -Scriptblock {RUNDLL32 PRINTUI.DLL,PrintUIEntry /ga /n\\$($P.Server+"\"+$P.Name) /q}
+        }
+    }
+}
+
+if ($borre -contains $username) {
+    $Printer = $Printers | Where-Object {$_.school -eq 'borre'} 
+    foreach ($p in $Printer) {
+        if ($Printers.Name -contains $Printer.Name) {
+            Write-Host "$($p.name) Exist, Skipping"
+        }
+        else {
+            Invoke-Command -Scriptblock {RUNDLL32 PRINTUI.DLL,PrintUIEntry /ga /n\\$($P.Server+"\"+$P.Name) /q}
+        }
+    }
+}
+
+if ($oreronningen -contains $username) {
+    $Printer = $Printers | Where-Object {$_.school -eq 'oreronningen'} 
+    foreach ($p in $Printer) {
+        if ($Printers.Name -contains $Printer.Name) {
+            Write-Host "$($p.name) Exist, Skipping"
+        }
+        else {
+            Invoke-Command -Scriptblock {RUNDLL32 PRINTUI.DLL,PrintUIEntry /ga /n\\$($P.Server+"\"+$P.Name) /q}
+        }
+    }
+}
+if ($Username -notlike '*eksamen*') {
+    $Printer = $Printers
+    foreach ($p in $Printer) {
+        Invoke-Command -Scriptblock {RUNDLL32 PRINTUI.DLL,PrintUIEntry /gd /n\\$($P.Server+"\"+$P.Name) /q}
+    }
+}
