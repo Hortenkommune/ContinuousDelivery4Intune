47bd40f9b1f7efb18ddb2cc3529ccf4a42a960b6
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-05-03 11:51:07 +0200

    added slp

--- resources/scripts/Install-EksamenPrinters.ps1
+++ resources/scripts/Install-EksamenPrinters.ps1
@@ -1,3 +1,5 @@
+Start-Sleep -Seconds 30
+
 $Username = Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username
 $Username = $Username.Replace("SKOLE\", "")
 $InstalledPrinters = Get-Printer
