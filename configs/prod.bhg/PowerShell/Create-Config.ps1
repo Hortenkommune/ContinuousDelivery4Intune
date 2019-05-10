a7c05cfdfa2c5b55bd2bdf5f22da1fb093b553ab
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-05-10 10:40:31 +0200

    ISR

--- configs/prod.bhg/PowerShell/Create-Config.ps1
+++ configs/prod.bhg/PowerShell/Create-Config.ps1
@@ -8,6 +8,11 @@
         Name      = "Add Restart-Computer every night"
         Command   = "Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-Command Restart-Computer -Force') -Trigger (New-ScheduledTaskTrigger -Daily -At 09:00pm) -User 'SYSTEM' -RunLevel Highest -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -WakeToRun) -TaskName 'Nightly Reboot' -Description 'Restart Computer nightly'"
         Detection = "[bool](Get-ScheduledTask -TaskName 'Nightly Reboot')"
+    },
+    @{
+        Name      = "Remove desktop.ini from OneDrive"
+        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'SKOLE\\', ''); Remove-Item ('C:\Users\' + `$Username + '\OneDrive - Horten kommune\desktop.ini') -Force"
+        Detection = "`$Username = (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); `$Username = `$Username -replace `"SKOLE\\`",`"`";[bool](!(Test-Path `"C:\Users\`$Username\OneDrive - Horten kommune\desktop.ini`"))"
     }
 )
 $PowerShell | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
