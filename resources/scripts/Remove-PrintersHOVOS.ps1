6e243e30d2334b6b354cba7542f2a1780546ab06
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-05-21 09:52:16 +0200

    fix2

--- resources/scripts/Remove-PrintersHOVOS.ps1
+++ resources/scripts/Remove-PrintersHOVOS.ps1
@@ -2,7 +2,7 @@ $Printers = Get-Printer
 $PrintersToRm = @('Rom 204 HOVOS', 'Rom 203 HOVOS', 'Rom 121 HOVOS', 'Rom 120 HOVOS')
 
 Foreach ($Printer in $PrintersToRm) {
-    If ($Printers.Name -contains $Printer.Name) {
+    If ($Printers.Name -contains $Printer) {
         Remove-Printer -Name $Printer
         Remove-PrinterPort -Name $Printer
     }
