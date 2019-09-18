541fe2a038fb5f91f578bf1bff96d3fd6e8c0103
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2019-09-18 11:34:48 +0200

    added uniflow

--- configs/beta/Custom Execution/Create-Config.ps1
+++ configs/beta/Custom Execution/Create-Config.ps1
@@ -140,6 +140,31 @@
                 Rule = "if((Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username) -like `"*eksamen*`"){`$Install = `$true}elseif(Get-Printer | Where-Object {@('\\10.85.207.8\2FL02588','\\10.85.207.8\2FL07038','\\10.85.207.8\QLC31644','\\10.85.207.8\XVC08019','\\10.85.207.8\XVF14345','\\10.85.207.8\QNW11407') -contains `$_.Name }) {`$Install = `$true};[bool](!(`$Install -eq `$true))"
             }
         )
+    },
+    @{
+        Name           = "Install uniFlow printer"
+        wrkDir         = "C:\Windows\Temp"
+        FilesToDwnload = @(
+            @{
+                FileName = "Install-UniflowPrinter.ps1"
+                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/scripts/Install-UniflowPrinter.ps1"
+            },
+            @{
+                FileName = "uniflow_pclxl.zip"
+                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/bin/uniflow_pclxl.zip"
+            }
+        )
+        Execution      = @(
+            @{
+                Execute   = "powershell.exe"
+                Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Install-UniflowPrinter.ps1"
+            }
+        )
+        Detection      = @(
+            @{
+                Rule = ""
+            }
+        )
     }
 )
 
---
+++ resources/scripts/Install-UniflowPrinter.ps1
@@ -0,0 +1,38 @@
+﻿#Get UPN
+$Username = Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username
+$ObjUser = New-Object System.Security.Principal.NTAccount($Username)
+$SID = $Objuser.Translate([System.Security.Principal.SecurityIdentifier])
+$UPN = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\IdentityStore\Cache\$($SID.value)\IdentityCache\$($SID.value)" -Name "UserName"
+
+#Print Driver
+$ZipFile = "C:\Windows\Temp\uniflow_pclxl.zip"
+Expand-Archive -Path $ZipFile -DestinationPath "$env:PROGRAMFILES\PrintDriver" -Force
+$InstallDriverPath = "$env:PROGRAMFILES\PrintDriver\MomUdPclXl.inf"
+Start-Process -FilePath "pnputil.exe" -ArgumentList "/Add-Driver `"$InstallDriverPath`"" -Wait
+
+#Printer Object
+$Printer = [PSCustomObject]@{
+    Name           = "HortenPrintElev"
+    PortName       = "HortenPrintElev"
+    ServerHost     = "10.82.24.122"
+    LprQueueName   = "HortenPrintElev"
+    DriverName     = "uniFLOW Universal PCLXL Driver"
+    PrintProcessor = "winprint"
+}
+
+#Installing Printer
+Add-PrinterDriver -Name $Printer.DriverName
+Add-PrinterPort -Name $Printer.PortName -LprHostAddress $Printer.ServerHost -LprQueueName $Printer.LprQueueName -LprByteCounting
+Add-Printer -Name $Printer.Name -PortName $Printer.PortName -DriverName $Printer.DriverName -PrintProcessor $Printer.PrintProcessor
+
+#Registry settings for CurrentUser
+New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+New-Item -Path "HKU:\$($SID.Value)\Software\Wow6432Node\NT-Ware\MOMUD" -Name "UPN" -Value $UPN -Force
+
+#Registry settings for LocalMachine
+$HKLMValue = "UPN=%Registry.HKEY_CURRENT_USER\SOFTWARE\Wow6432Node\NT-Ware\MOMUD\UPN%"
+New-Item -Path "HKLM:\SOFTWARE\Nt-ware\MOMUD" -Name "AltUserIdent" -Value $HKLMValue -Force
+New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Nt-ware\MOMUD" -Name "AltUserIdent" -Value $HKLMValue -Force
+New-Item -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)\PrinterDriverData" -Name "Port" -Value "8000" -Force
+New-Item -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)\PrinterDriverData" -Name "Url" -Value "/pwclient/isapi/IcarusRequest.dll?script=showudoptions.icarus" -Force
+Set-ItemProperty -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)" -Name "Print Processor" -Value "winprint" -Force
