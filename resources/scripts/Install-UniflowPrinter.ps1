0dc32d4808abaf6e4d2ab325346480167a846908
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2019-09-18 11:56:17 +0200

    fixed some things

--- resources/scripts/Install-UniflowPrinter.ps1
+++ resources/scripts/Install-UniflowPrinter.ps1
@@ -27,12 +27,17 @@ Add-Printer -Name $Printer.Name -PortName $Printer.PortName -DriverName $Printer
 
 #Registry settings for CurrentUser
 New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
-New-Item -Path "HKU:\$($SID.Value)\Software\Wow6432Node\NT-Ware\MOMUD" -Name "UPN" -Value $UPN -Force
+New-Item -Path "HKU:\$($SID.Value)\Software\Wow6432Node\NT-Ware\MOMUD" -Force
+New-ItemProperty -Path "HKU:\$($SID.Value)\Software\Wow6432Node\NT-Ware\MOMUD" -Name "UPN" -Value $UPN -Force
 
 #Registry settings for LocalMachine
 $HKLMValue = "UPN=%Registry.HKEY_CURRENT_USER\SOFTWARE\Wow6432Node\NT-Ware\MOMUD\UPN%"
-New-Item -Path "HKLM:\SOFTWARE\Nt-ware\MOMUD" -Name "AltUserIdent" -Value $HKLMValue -Force
-New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Nt-ware\MOMUD" -Name "AltUserIdent" -Value $HKLMValue -Force
-New-Item -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)\PrinterDriverData" -Name "Port" -Value "8000" -Force
-New-Item -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)\PrinterDriverData" -Name "Url" -Value "/pwclient/isapi/IcarusRequest.dll?script=showudoptions.icarus" -Force
-Set-ItemProperty -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)" -Name "Print Processor" -Value "winprint" -Force
+
+New-Item -Path "HKLM:\SOFTWARE\Nt-ware" -Name "MOMUD" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Nt-ware\MOMUD" -Name "AltUserIdent" -Value $HKLMValue -PropertyType "String" -Force
+New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Nt-ware" -Name "MOMUD" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Nt-ware\MOMUD" -Name "AltUserIdent" -Value $HKLMValue -PropertyType "String" -Force
+
+New-ItemProperty -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)\PrinterDriverData\" -Name "Port" -Value "8000" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)\PrinterDriverData\" -Name "Url" -Value "/pwclient/isapi/IcarusRequest.dll?script=showudoptions.icarus" -PropertyType "String" -Force
+Set-ItemProperty -Path "HKLM:\System\ControlSet001\Control\Print\Printers\$($Printer.Name)\" -Name "Print Processor" -Value "winprint" -Force
