63072809c3b6dea9d5565300fa31870e6a146f6c
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2019-09-19 18:37:49 +0200

    rm printkode SC

--- configs/prod.bs/Shortcuts/Create-Config.ps1
+++ configs/prod.bs/Shortcuts/Create-Config.ps1
@@ -74,7 +74,7 @@
         WorkingDir      = "C:\Program Files\internet explorer\"
         IconFileandType = "C:\Windows\System32\imageres.dll, 46"
         Description     = "Printkode"
-        Mode            = "Install"
+        Mode            = "Uninstall"
     },
     @{
         Name            = "Veiledninger"
