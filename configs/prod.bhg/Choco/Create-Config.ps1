90bbf4f5025e4ac368abdf68c458c2c1f2fcb37f
Nikolai Poverud <nikpov@intern.i-sone.no>
2019-10-21 16:34:59 +0200

    spotify bhg

--- configs/prod.bhg/Choco/Create-Config.ps1
+++ configs/prod.bhg/Choco/Create-Config.ps1
@@ -18,6 +18,10 @@
     @{
         Name = "dotnet3.5"
         Mode = "install"
+    },
+    @{
+        Name = "spotify"
+        Mode = "install"
     }
 )
 $ChocoPkgs | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
