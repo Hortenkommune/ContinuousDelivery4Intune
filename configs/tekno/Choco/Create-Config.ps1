d69e8431101ed2705264b2b4792eb4e7cb4d2035
Nikolai Poverud <nikpov@intern.i-sone.no>
2019-10-30 15:34:44 +0100

    added choco apps

--- configs/tekno/Choco/Create-Config.ps1
+++ configs/tekno/Choco/Create-Config.ps1
@@ -1,23 +1,11 @@
 ﻿$ChocoPkgs = @(
     @{
+        Name = "arduino"
+        Mode = "install"
+    },
+    @{
         Name = "googlechrome"
         Mode = "install"
-    },
-    @{
-        Name = "gimp"
-        Mode = "install"
-    },
-    @{
-        Name = "audacity"
-        Mode = "install"
-    },
-    @{
-        Name = "sccmtoolkit"
-        Mode = "install"
-    },
-    @{
-        Name = "dotnet3.5"
-        Mode = "install"
-    }  
+    }
 )
 $ChocoPkgs | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
