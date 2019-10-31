05dfa6673101941074c5de65d34e206b44d5fc83
Nikolai Poverud <nikpov@intern.i-sone.no>
2019-10-31 16:03:30 +0100

    more cleanup

--- configs/tekno/Shortcuts/Create-Config.ps1
+++ configs/tekno/Shortcuts/Create-Config.ps1
@@ -1,15 +1,5 @@
 ﻿$Shortcuts = @(
     @{
-        Name            = "Google Earth"
-        Type            = "lnk"
-        Path            = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-        Arguments       = "https://earth.google.com/web"
-        WorkingDir      = "C:\Program Files (x86)\Google\Chrome\Application"
-        IconFileandType = "C:\Windows\ICO\google_earth.ico"
-        Description     = "Google Earth Cloud"
-        Mode            = "Install"
-    },
-    @{
         Name            = "Printkode"
         Type            = "lnk"
         Path            = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
@@ -17,16 +7,6 @@
         WorkingDir      = "C:\Program Files (x86)\Google\Chrome\Application"
         IconFileandType = "C:\Windows\System32\imageres.dll, 46"
         Description     = "Printkode"
-        Mode            = "Install"
-    },
-    @{
-        Name            = "Veiledninger"
-        Type            = "lnk"
-        Path            = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-        Arguments       = "https://info.hortenskolen.no"
-        WorkingDir      = "C:\Program Files (x86)\Google\Chrome\Application"
-        IconFileandType = "C:\Windows\System32\imageres.dll, 76"
-        Description     = "Diverse veiledinger"
         Mode            = "Install"
     }
 )
