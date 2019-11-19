a143db25f7599cb490bdde11f815afcf96240e50
Nikolai Poverud <nikpov@intern.i-sone.no>
2019-11-19 10:47:25 +0100

    added python, processing tekno

--- configs/tekno/Choco/Create-Config.ps1
+++ configs/tekno/Choco/Create-Config.ps1
@@ -7,5 +7,13 @@
         Name = "googlechrome"
         Mode = "install"
     }
+    @{
+        Name = "python"
+        Mode = "Install"
+    },
+    @{
+        Name = "processing"
+        Mode = "Install"
+    }
 )
 $ChocoPkgs | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
