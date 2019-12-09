a6c86e38700796776c644ec0d2fe29d334e381ff
Nikolai Poverud <nikpov@intern.i-sone.no>
2019-12-09 13:50:48 +0100

    removed bhg printer

--- configs/prod.bhg/Custom Execution/Create-Config.ps1
+++ configs/prod.bhg/Custom Execution/Create-Config.ps1
@@ -1,28 +1,2 @@
-﻿$CustomExec = @(
-    @{
-        Name           = "Deploy printers"
-        wrkDir         = "C:\Windows\Temp"
-        FilesToDwnload = @(
-            @{
-                FileName = "Install-BHGPrinters.ps1"
-                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/scripts/Install-BHGPrinters.ps1"
-            },
-            @{
-                FileName = "gpb0.zip"
-                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/bin/gpb0.zip"
-            }
-        )
-        Execution      = @(
-            @{
-                Execute   = "powershell.exe"
-                Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Install-BHGPrinters.ps1"
-            }
-        )
-        Detection      = @(
-            @{
-                Rule = "[bool](Get-WmiObject -Query `"select * from win32_printer where name like '%Printer%'`")"
-            }
-        )
-    }
-)
+﻿$CustomExec = $null
 $CustomExec | ConvertTo-Json -Depth 4 -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
