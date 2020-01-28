44b7215b5d7c16fbab0fdce68b11ae022dd06553
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2020-01-28 11:29:35 +0100

    upped main version to 1.0.11

--- versioncontrol/Create-Config.ps1
+++ versioncontrol/Create-Config.ps1
@@ -1,31 +1,31 @@
 ﻿$Versions = @(
     @{
         Name    = "prod.bs"
-        Version = "1.0.5"
+        Version = "1.0.11"
     },
     @{
         Name    = "prod.us"
-        Version = "1.0.5"
+        Version = "1.0.11"
     },
     @{
         Name    = "prod.hovos"
-        Version = "1.0.5"
+        Version = "1.0.11"
     },
     @{
         Name    = "prod.bhg"
-        Version = "1.0.6"
+        Version = "1.0.11"
     },
     @{
         Name    = "prod.bakk"
-        Version = "1.0.5"
+        Version = "1.0.11"
     },
     @{
         Name    = "beta"
-        Version = "1.0.10"
+        Version = "1.0.11"
     },
     @{
         Name    = "tekno"
-        Version = "1.0.5"
+        Version = "1.0.11"
     }
 )
 $Versions | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
