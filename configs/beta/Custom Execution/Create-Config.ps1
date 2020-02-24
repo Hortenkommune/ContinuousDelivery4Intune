0c520804e1ac234d733ae87b741a6923d2ae96c4
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2020-02-24 13:09:05 +0100

    tyop

--- configs/beta/Custom Execution/Create-Config.ps1
+++ configs/beta/Custom Execution/Create-Config.ps1
@@ -137,7 +137,7 @@
         )
         Detection       = @(
             @{
-                Rule = "[bool](!(Get-PrinterDriver -Name `"Canon Generic Plus UFR II`"))"
+                Rule = "[bool](Get-PrinterDriver -Name `"Canon Generic Plus UFR II`")"
             }
         )
     }
