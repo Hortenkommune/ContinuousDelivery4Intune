02dc34f98e6cda3c1cdbfce37b1f26fc5b297d68
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2020-02-24 12:46:24 +0100

    jonas mergan

--- configs/beta/Custom Execution/Create-Config.ps1
+++ configs/beta/Custom Execution/Create-Config.ps1
@@ -115,6 +115,31 @@
                 Rule = "if((Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username) -like `"*eksamen*`"){`$Install = `$true}elseif(Get-Printer | Where-Object {@('\\10.85.207.8\2FL02588','\\10.85.207.8\2FL07038','\\10.85.207.8\QLC31644','\\10.85.207.8\XVC08019','\\10.85.207.8\XVF14345','\\10.85.207.8\QNW11407') -contains `$_.Name }) {`$Install = `$true};[bool](!(`$Install -eq `$true))"
             }
         )
+    },
+    @{
+        Name            = "Install Print Driver"
+        wrkDir         = "C:\Windows\Temp"
+        FilesToDwnload = @(
+            @{
+                FileName = "Install-UFRIIDriver.ps1"
+                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/scripts/Install-UFRIIDriver.ps1"
+            },
+            @{
+                FileName = "cnlb0m.zip"
+                URL      = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/bin/cnlb0m.zip"
+            }
+        )
+        Execution       = @(
+            @{
+                Execute   = "powershell.exe"
+                Arguments = "-ExecutionPolicy Bypass -File C:\Windows\Temp\Install-UFRIIDriver.ps1"
+            }
+        )
+        Detection       = @(
+            @{
+                Rule = "[bool](!(Get-PrinterDriver -Name `"Canon Generic Plus UFR II`"))"
+            }
+        )
     }
 )
 
