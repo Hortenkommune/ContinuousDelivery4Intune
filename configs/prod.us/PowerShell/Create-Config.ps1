f6f31cc130b8d97b67b4d12dade0bb39185b33fb
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2022-08-26 14:37:05 +0200

    Added Disable Acer Quick Access to prod

--- configs/prod.us/PowerShell/Create-Config.ps1
+++ configs/prod.us/PowerShell/Create-Config.ps1
@@ -38,6 +38,11 @@
         Name = "Pin Audacity to 2.4.2"
         Command = "choco pin add -n=audacity --version 2.4.2"
         Detection = "`$false"
+    },
+    @{
+        Name = "Disable Acer Quick Access"
+        Command = "Get-ScheduledTask -TaskName 'Software Update Application' | Disable-ScheduledTask"
+        Detection = "[bool](Get-ScheduledTask -TaskName 'Software Update Application' | Where-Object {`$_.State -eq 'Disabled'})"
     }
 )
-$PowerShell | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
+$PowerShell | ConvertTo-Json -Compress -EscapeHandling EscapeHtml | Out-File "$PSScriptRoot\config.json" -Encoding default
