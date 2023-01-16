934c976505f19b8ad38e381d6b6569e32a1faa51
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2023-01-16 15:45:19 +0100

    commenting oldstuff

--- configs/beta/PowerShell/Create-Config.ps1
+++ configs/beta/PowerShell/Create-Config.ps1
@@ -1,9 +1,9 @@
 ﻿$PowerShell = @(
-    @{
-        Name      = "Remove VNC from Start Menu"
-        Command   = "Remove-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC' -Recurse -Force"
-        Detection = "[bool](!(Test-Path -Path `"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC`"))"
-    },
+    #@{
+    #    Name      = "Remove VNC from Start Menu"
+    #    Command   = "Remove-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC' -Recurse -Force"
+    #    Detection = "[bool](!(Test-Path -Path `"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC`"))"
+    #},
     #@{
     #    Name      = "Add Restart-Computer every night"
     #    Command   = "Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-Command Restart-Computer -Force') -Trigger (New-ScheduledTaskTrigger -Daily -At 09:00pm) -User 'SYSTEM' -RunLevel Highest -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -WakeToRun) -TaskName 'Nightly Reboot' -Description 'Restart Computer nightly'"
@@ -39,16 +39,16 @@
         Command   = "Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 -Online -NoRestart" 
         Detection = "[bool]((Get-WindowsOptionalFeature -Online | Where-Object {`$_.FeatureName -eq 'Internet-Explorer-Optional-amd64'}).State -eq 'Disabled')"
     },
-    @{
-        Name      = "Run DevCon64"
-        Command   = "Start-Process `"devcon64`" -ArgumentList `"update`",`"C:\windows\inf\hdaudio.inf`",'\`"`"HDAUDIO\FUNC_01&VEN_10EC&DEV_0283\`"`"'"
-        Detection = "[bool](!(Get-WmiObject -Query `"select * from win32_computersystem where model like '20DA%'`")) -or ((Get-PnpDevice `"HDAUDIO\FUNC_01&VEN_10EC&DEV_0283*`").FriendlyName -like `"High Definition*`")"
-    },
-    @{
-        Name      = "Pin Audacity to 2.4.2"
-        Command   = "choco pin add -n=audacity --version 2.4.2"
-        Detection = "`$false"
-    },
+    #@{
+    #    Name      = "Run DevCon64"
+    #    Command   = "Start-Process `"devcon64`" -ArgumentList `"update`",`"C:\windows\inf\hdaudio.inf`",'\`"`"HDAUDIO\FUNC_01&VEN_10EC&DEV_0283\`"`"'"
+    #    Detection = "[bool](!(Get-WmiObject -Query `"select * from win32_computersystem where model like '20DA%'`")) -or ((Get-PnpDevice `"HDAUDIO\FUNC_01&VEN_10EC&DEV_0283*`").FriendlyName -like `"High Definition*`")"
+    #},
+    #@{
+    #    Name      = "Pin Audacity to 2.4.2"
+    #    Command   = "choco pin add -n=audacity --version 2.4.2"
+    #    Detection = "`$false"
+    #},
     @{
         Name      = "Disable Acer Quick Access"
         Command   = "Get-ScheduledTask -TaskName 'Software Update Application' | Disable-ScheduledTask"
