44225747ac17913af628137c39db8b27acb508bb
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2023-03-02 13:48:06 +0100

    removed apps from config migrating to winget

--- configs/beta/Choco/Create-Config.ps1
+++ configs/beta/Choco/Create-Config.ps1
@@ -3,34 +3,34 @@
     #     Name = "googlechrome"
     #     Mode = "uninstall"
     # },
-    @{
-        Name = "microsoft-teams.install"
-        Mode = "install"
-    },
-    @{
-        Name = "audacity --version=3.0.0"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=3.0.1"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=3.0.2"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=3.0.3"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=2.4.2 --allow-downgrade"
-        Mode = "install"
-    },
-    @{
-        Name = "sccmtoolkit"
-        Mode = "install"
-    },
+    # @{
+    #     Name = "microsoft-teams.install"
+    #     Mode = "install"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.0"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.1"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.2"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.3"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=2.4.2 --allow-downgrade"
+    #     Mode = "install"
+    # },
+    # @{
+    #     Name = "sccmtoolkit"
+    #     Mode = "install"
+    # },
     # @{
     #     Name = "dotnet3.5"
     #     Mode = "install"
@@ -39,9 +39,9 @@
     #     Name = "devcon.portable"
     #     Mode = "install"
     # },
-    @{
-        Name = "dotnet"
-        Mode = "install"
-    }
+    # @{
+    #     Name = "dotnet"
+    #     Mode = "install"
+    # }
 )
 $ChocoPkgs | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
