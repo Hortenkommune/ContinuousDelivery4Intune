1a5d8a03d3fff6fe91ee5cc23c62af6924dce37d
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2023-06-26 09:05:38 +0200

    disabling choco

--- configs/prod.bs/Choco/Create-Config.ps1
+++ configs/prod.bs/Choco/Create-Config.ps1
@@ -1,47 +1,47 @@
 ﻿$ChocoPkgs = @(
-    @{
-        Name = "sccmtoolkit"
-        Mode = "install"
-    },    
-    @{
-        Name = "microsoft-teams.install"
-        Mode = "install"
-    },
-    @{
-        Name = "googlechrome"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=3.0.0"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=3.0.1"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=3.0.2"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=3.0.3"
-        Mode = "uninstall"
-    },
-    @{
-        Name = "audacity --version=2.4.2 --allow-downgrade"
-        Mode = "install"
-    },
-    @{
-        Name = "dotnet3.5"
-        Mode = "install"
-    },
-    @{
-        Name = "devcon.portable"
-        Mode = "install"
-    },
-    @{
-        Name = "dotnet"
-        Mode = "install"
-    }
+    # @{
+    #     Name = "googlechrome"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "microsoft-teams.install"
+    #     Mode = "install"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.0"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.1"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.2"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=3.0.3"
+    #     Mode = "uninstall"
+    # },
+    # @{
+    #     Name = "audacity --version=2.4.2 --allow-downgrade"
+    #     Mode = "install"
+    # },
+    # @{
+    #     Name = "sccmtoolkit"
+    #     Mode = "install"
+    # },
+    # @{
+    #     Name = "dotnet3.5"
+    #     Mode = "install"
+    # },
+    # @{
+    #     Name = "devcon.portable"
+    #     Mode = "install"
+    # },
+    # @{
+    #     Name = "dotnet"
+    #     Mode = "install"
+    # }
 )
 $ChocoPkgs | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
