cce35e79082e12dd88b31ee6eb789e70b2d5f061
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2023-09-18 14:41:10 +0200

    trying to fix detection

--- configs/beta/PowerShell/Create-Config.ps1
+++ configs/beta/PowerShell/Create-Config.ps1
@@ -12,7 +12,7 @@
     @{
         Name      = "Add Teams Shortcut"
         Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'SKOLE\\', ''); Copy-Item -Path ('C:\Users\' + $Username + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams (work or school).lnk') -Destination ('C:\Users\' + $Username + '\Desktop')"
-        Detection = "`$Username = (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); `$Username = `$Username -replace `"SKOLE\\`",`"`";[bool](!(Test-Path `"C:\Users\`$Username\Desktop\'Microsoft Teams (work or school).lnk'`"))"
+        Detection = "`$Username = (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); `$Username = `$Username -replace `"SKOLE\\`",`"`";[bool]((Test-Path `"C:\Users\`$Username\Desktop\'Microsoft Teams (work or school).lnk'`"))"
     },
     @{
         Name      = "Disable Restart-Computer every night"
