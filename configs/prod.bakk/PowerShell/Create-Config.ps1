bc448d227b2d518d56d4c34f266f0cc28c8b1588
Per-Ole Fanuelsen <perfan@hortenkommune365.onmicrosoft.com>
2023-12-11 11:50:07 +0100

    updated powershell

--- configs/prod.bakk/PowerShell/Create-Config.ps1
+++ configs/prod.bakk/PowerShell/Create-Config.ps1
@@ -3,6 +3,17 @@
         Name = "Disable Acer Quick Access"
         Command = "Get-ScheduledTask -TaskName 'Software Update Application' | Disable-ScheduledTask"
         Detection = "[bool](Get-ScheduledTask -TaskName 'Software Update Application' | Where-Object {`$_.State -eq 'Disabled'})"
+        
+    }
+    @{
+        Name      = "Remove Teams Shortcut"
+        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Remove-Item -Path ('C:\Users\' + `$Username + '\Desktop\Microsoft Teams (work or school).lnk')"
+        Detection = "`$false"
+    },
+    @{
+        Name      = "Remove Teams Shortcut"
+        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Remove-item -Path ('C:\Users\' + `$Username + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams classic (work or school).lnk')"
+        Detection = "`$false"
     }
 )
 $PowerShell | ConvertTo-Json -Compress -EscapeHandling EscapeHtml | Out-File "$PSScriptRoot\config.json" -Encoding default
--- configs/prod.hovos/PowerShell/Create-Config.ps1
+++ configs/prod.hovos/PowerShell/Create-Config.ps1
@@ -1,8 +1,13 @@
 ﻿$PowerShell = @(
     @{
-        Name      = "Disable Restart-Computer every night"
-        Command   = "Get-ScheduledTask -TaskName 'Nightly Reboot' | Disable-ScheduledTask"
-        Detection = "[bool](Get-ScheduledTask -TaskName 'Nightly Reboot' | Where-Object {`$_.State -eq 'Disabled'})"
+        Name      = "Remove Teams Shortcut"
+        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Remove-Item -Path ('C:\Users\' + `$Username + '\Desktop\Microsoft Teams (work or school).lnk')"
+        Detection = "`$false"
+    },
+    @{
+        Name      = "Remove Teams Shortcut"
+        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Remove-item -Path ('C:\Users\' + `$Username + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams classic (work or school).lnk')"
+        Detection = "`$false"
     },
     @{
         Name      = "Remove desktop.ini from OneDrive"
