d3cdf33a9654755710c7a79b3eab3c233a766d69
Per-Ole Fanuelsen <perfan@hortenkommune365.onmicrosoft.com>
2023-12-11 10:48:20 +0100

    Using wrong cmdlet

--- configs/beta/PowerShell/Create-Config.ps1
+++ configs/beta/PowerShell/Create-Config.ps1
@@ -16,12 +16,12 @@
     },
     @{
         Name      = "Remove Teams Shortcut"
-        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Delete-Item -Path ('C:\Users\' + `$Username + '\Desktop\Microsoft Teams (work or school).lnk')"
+        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Remove-Item -Path ('C:\Users\' + `$Username + '\Desktop\Microsoft Teams (work or school).lnk')"
         Detection = "`$false"
     },
     @{
         Name      = "Remove Teams Shortcut"
-        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Delete-Item -Path ('C:\Users\' + `$Username + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams classic (work or school).lnk')"
+        Command   = "New-Variable Username -Value (Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username); Set-Variable Username -Value (`$Username -replace 'AzureAD\\', ''); Set-Variable Username -Value (`$Username -replace 'Skole\\', ''); Remove-item -Path ('C:\Users\' + `$Username + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams classic (work or school).lnk')"
         Detection = "`$false"
     },
     @{
