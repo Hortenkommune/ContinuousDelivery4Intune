66ec5b18934f275699839d71d20ea1bcdb79ce18
Per-Ole Fanuelsen <perfan@hortenkommune365.onmicrosoft.com>
2023-12-11 11:01:17 +0100

    added new teams icon

--- configs/beta/IconsCfg/Create-Config.ps1
+++ configs/beta/IconsCfg/Create-Config.ps1
@@ -5,6 +5,11 @@
         Mode = "Install"
     },
     @{
+        URI  = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/ico/teams.ico"
+        Name = "new_teams.ico"
+        Mode = "Install"
+    },
+    @{
         URI  = "https://raw.githubusercontent.com/Hortenkommune/ContinuousDelivery4Intune/master/resources/ico/Office_portal.ico"
         Name = "Office_portal.ico"
         Mode = "Install"
