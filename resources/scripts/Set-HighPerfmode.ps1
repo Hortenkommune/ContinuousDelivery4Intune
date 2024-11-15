e9f19aa0f4aa42a2794a5407d13517901df588cd
fanuelsen <perfan@hortenkommune365.onmicrosoft.com>
2024-11-15 14:49:19 +0100

    fixed script to check if property exist

--- resources/scripts/Set-HighPerfmode.ps1
+++ resources/scripts/Set-HighPerfmode.ps1
@@ -6,11 +6,19 @@ $Properties = @(
     @{ Name = "ActiveOverlayAcPowerScheme"; Value = "ded574b5-45a0-4f42-8737-46345c09c238" }
 )
 
+
 foreach ($Property in $Properties) {
-    if (-not (Get-ItemProperty -Path $RegistryPath -Name $Property.Name -ErrorAction SilentlyContinue)) {
+    try {
+        $CurrentValue = Get-ItemProperty -Path $RegistryPath -Name $Property.Name -ErrorAction SilentlyContinue | Select-Object -ExpandProperty $($Property.Name)
+        
+        if ($CurrentValue -ne $Property.Value) {
+            Set-ItemProperty -Path $RegistryPath -Name $Property.Name -Value $Property.Value
+            Write-Host "Registry key '$($Property.Name)' updated with value '$($Property.Value)' in path '$RegistryPath'."
+        } else {
+            Write-Host "Registry key '$($Property.Name)' already has the correct value."
+        }
+    } catch {
         Set-ItemProperty -Path $RegistryPath -Name $Property.Name -Value $Property.Value
         Write-Host "Registry key '$($Property.Name)' added with value '$($Property.Value)' in path '$RegistryPath'."
-    } else {
-        Write-Host "Registry key '$($Property.Name)' already exists."
     }
 }
