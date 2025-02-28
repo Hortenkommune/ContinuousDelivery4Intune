691b1229bed6081d900aa0ed88069e2733ef74f9
Per-Ole <fanuelsen@mac.com>
2025-02-28 13:27:01 +0100

    added adapit to hovos branch

--- configs/prod.hovos/Shortcuts/Create-Config.ps1
+++ configs/prod.hovos/Shortcuts/Create-Config.ps1
@@ -85,6 +85,16 @@
         IconFileandType = "C:\Windows\ICO\new_teams.ico"
         Description     = "Microsoft Teams"
         Mode            = "Install"
+    },
+    @{
+        Name            = "Adapt-IT"
+        Type            = "lnk"
+        Path            = "C:\Windows\explorer.exe"
+        Arguments       = "microsoft-edge:`"https://adaptit.enovate.no/adapt-it/Login`""
+        WorkingDir      = ""
+        IconFileandType = "C:\Windows\ICO\adaptit.ico"
+        Description     = "Adapt-IT"
+        Mode            = "Install"
     }
 )
 $Shortcuts | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
